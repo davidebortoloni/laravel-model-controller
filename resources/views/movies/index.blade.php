2537473cba9dd62a5c12d30b1e85416c3c8a4042
davidebortoloni <davidebortoloni@gmail.com>
2021-10-08 09:47:17 +0200

    Eliminazione passaggio dati a card

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -7,7 +7,7 @@
         <div class="container">
             <ul class="list-unstyled">
                 @forelse ($movies as $movie)
-                    <li>@include('includes.card', compact('movie'))</li>
+                    <li>@include('movies.card')</li>
                 @empty
                     <li><h2 class="text-center">Nessun film trovato</h2></li>
                 @endforelse
